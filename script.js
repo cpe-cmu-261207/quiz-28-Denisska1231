ab0e5b3bf51454eb3bdc34b8e646d4f4f318be5a
Denisska1231 <90751218+Denisska1231@users.noreply.github.com>
2022-08-04 14:39:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const axios = require("axios");
+callApi();
+
+const genderTag = document.getElementById("span-gender-icon");
+const imgTag = document.getElementById("img-profile");
+const nameTag = document.getElementById("p-name");
+const addressTag = document.getElementById("p-address");
+const emailTag = document.getElementById("p-email");
+const btnRandom = document.getElementById("btn-random");
+const maincard = document.getElementById("div-user-card");
+const loadcard = document.getElementById("div-loading-card");
+
+btnRandom.onclick = () => {
+  callApi();
+  maincard.style.display = "";
+  loadcard.style.display = "none";
+};
 
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
   return resp.data.results[0].picture.large;
 }
-callApi();
+
+
