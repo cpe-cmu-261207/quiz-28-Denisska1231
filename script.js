1cf174bd533d76ea0fb83dd6d99436b64cb91026
Denisska1231 <90751218+Denisska1231@users.noreply.github.com>
2022-08-04 14:21:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const axios = require("axios");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  return resp.data.results[0].picture.large;
 }
+callApi();
