4ec2fc2bf85c3e60b7d9732ead2609d855709775
Denisska1231 <90751218+Denisska1231@users.noreply.github.com>
2022-08-04 14:53:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 callApi();
 
-const genderTag = document.getElementById("span-gender-icon");
-const imgTag = document.getElementById("img-profile");
-const nameTag = document.getElementById("p-name");
-const addressTag = document.getElementById("p-address");
-const emailTag = document.getElementById("p-email");
-const btnRandom = document.getElementById("btn-random");
-const maincard = document.getElementById("div-user-card");
-const loadcard = document.getElementById("div-loading-card");
+const gen = document.getElementById("span-gender-icon");
+const img = document.getElementById("img-profile");
+const name= document.getElementById("p-name");
+const address = document.getElementById("p-address");
+const email = document.getElementById("p-email");
+const btnrandom = document.getElementById("btn-random");
+const user = document.getElementById("div-user-card");
+const loading = document.getElementById("div-loading-card");
 
-btnRandom.onclick = () => {
+btnrandom.onclick = () => {
   callApi();
-  maincard.style.display = "";
-  loadcard.style.display = "none";
+  user.style.display = "";
+  load.style.display = "none";
 };
 
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
-  return resp.data.results[0].picture.large;
+  if (resp.data.results[0].gender == "male") {
+    gen.innerText = "👨👩";
+  } else {
+    gen.innerText = "👩";
+  }
+  img.src = resp.data.results[0].picture.large;
+  name.innerText =
+    resp.data.results[0].name.first + " " + resp.data.results[0].name.last;
+  email.innerText = resp.data.results[0].email;
+  address.innerText =
+    resp.data.results[0].location.city +
+    " " +
+    resp.data.results[0].location.state +
+    " " +
+    resp.data.results[0].location.country +
+    " " +
+    resp.data.results[0].location.postcode;
+  console.log(resp.data.results[0]);
+}
+
+btnrandom.onload = () => {
+  load.style.display = "none";
+  user.style.display = "";
+};
 }
 
 
